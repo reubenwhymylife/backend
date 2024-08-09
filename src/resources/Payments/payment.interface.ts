12da6b1f440939e1781d360de744a3e6b1bb17e5
Jude <aon@Jude-MacBook-Pro.local>
2024-08-09 01:54:36 -0700

    added more payment types

--- src/resources/Payments/payment.interface.ts
+++ src/resources/Payments/payment.interface.ts
@@ -3,6 +3,8 @@ import { TxnStatus } from "./payment.model";
 export enum paymentType {
   SUBSCRIPTION = "subscription",
   PURPOSEPLAN = "purpose-plan",
+  DAILYMESSAGE = "daily-message",
+  CONTENTPAYMENT = "content-payment"
 }
 
 export interface IPayment {
